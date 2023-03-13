1633cd34139f2ce3592ae6f578b8bd7006b1681a
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-13 20:29:42 +0100

    Added sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <div className='flex'>
+          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+            <Sidebar />
+          </div>
+
+          {/* client provider */}
+          <div className='bg-[#343541] flex-1'>{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import NewChat from "./NewChat";
+
+interface SidebarProps {}
+
+const Sidebar: FC<SidebarProps> = ({}) => {
+  return (
+    <div className='flex flex-col h-screen'>
+      <div>
+        <div className='flex-1 '>
+          <NewChat />
+          <div>{/* model selection */}</div>
+
+          {/* map through chats */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
